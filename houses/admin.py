310504b1aa8fe8e9bfd85e60117afe160ca67747
Wesley Kim <wesleykim@Wesleyui-MacBookAir.local>
2024-05-15 18:38:58 +0900

    #1~#4house app model, admin panel

---
+++ houses/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import House  # 같은 폴더에 있는 모델 파일에서 HOUSE 클래스 가져옴
+
+# Register your models here.
+
+
+@admin.register(House)  # 아래 어드민 패널을 하우스 모델을 위하한다는 설정
+class HouseAdmin(admin.ModelAdmin):
+    # pass  # admin 모델을 그대로 가져와 쓴다는 의미
+    list_display = ("name", "price_per_night", "address", "pets_allowed")
+    list_filter = ("price_per_night", "pets_allowed")
+    search_fields = ("address__startswith",)
