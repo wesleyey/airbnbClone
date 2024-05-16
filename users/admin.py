77dc68df25218cd30521c94396e4abb5e396e5d8
Wesley Kim <wesleykim@Wesleyui-MacBookAir.local>
2024-05-16 22:32:58 +0900

    ~#5.6 user mode

--- users/admin.py
+++ users/admin.py
@@ -8,4 +8,30 @@ from .models import User
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    pass
+    fieldsets = (
+        (
+            "Profile",
+            {
+                "fields": ("username", "password", "name", "email", "is_host"),
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (
+            "Importans Dates",
+            {
+                "fields": ("last_login", "date_joined"),
+            },
+        ),
+    )
+    list_display = ("username", "email", "name", "is_host")
