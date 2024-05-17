9045f74de8071eb872c707f5eb71a996222d06f2
Wesley Kim <wesleykim@Wesleyui-MacBookAir.local>
2024-05-17 19:46:26 +0900

    ~#6.3 user model and admin

---
+++ rooms/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+from .models import Room, Amenity
+
+
+# Register your models here.
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "address",
+        "price",
+        "owner",
+    )
+    list_filter = (
+        "country",
+        "city",
+        "pet_friendly",
+        "kind",
+        "amenities",
+    )
+
+
+@admin.register(Amenity)
+class AmenityAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "description",
+        "created_at",
+        "updated_at",
+    )
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
