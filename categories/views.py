8036cc8e1fbad0ae3e4238859e61e2b1b3aa32dc
Wesley Kim <wesleykim@Wesleyui-MacBookAir.local>
2024-05-24 15:47:56 +0900

    ~#10.9 how things works inside of api

--- categories/views.py
+++ categories/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import Category
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from rest_framework.status import HTTP_204_NO_CONTENT
 from .serializers import CategrySerializer
 
 # Create your views here.
@@ -18,13 +20,32 @@ def categories(request):
     elif request.method == "POST":
         serializer = CategrySerializer(data=request.data)
         if serializer.is_valid():
-            return Response({"created": True})
+            new_category = serializer.save()  # create method return value save
+            return Response(CategrySerializer(new_category).data)  # re-serialize!
         else:
             return Response(serializer.errors)
 
 
-@api_view()
+@api_view(["GET", "PUT", "DELETE"])
 def category(request, id):
-    category = Category.objects.get(id=id)
-    serializer = CategrySerializer(category)
-    return Response(serializer.data)
+    try:
+        category = Category.objects.get(id=id)
+    except Category.DoesNotExist:
+        raise NotFound
+    if request.method == "GET":
+        serializer = CategrySerializer(category)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = CategrySerializer(
+            category,
+            data=request.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            updated_category = serializer.save()
+            return Response(CategrySerializer(updated_category).data)
+        else:
+            return Response(serializer.errors)
+    elif request.method == "DELETE":
+        category.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
