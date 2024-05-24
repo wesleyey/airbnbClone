7f1e3027ad41c33da18f082cd2ecf7a02338a64b
Wesley Kim <wesleykim@Wesleyui-MacBookAir.local>
2024-05-24 22:56:10 +0900

    ~#10.13 modelserializer, modelviewset magic

--- categories/serializers.py
+++ categories/serializers.py
@@ -2,6 +2,15 @@ from rest_framework import serializers
 from .models import Category
 
 
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = "__all__"
+        # fields 요소 하나씩 열거, exclude 제외할 것 명시
+
+
+"""
 class CategrySerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(
@@ -21,3 +30,4 @@ class CategrySerializer(serializers.Serializer):
         instance.kind = validated_data.get("kind", instance.kind)
         instance.save()
         return instance
+"""
--- categories/views.py
+++ categories/views.py
@@ -1,14 +1,43 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from .models import Category
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.exceptions import NotFound
-from rest_framework.status import HTTP_204_NO_CONTENT
-from .serializers import CategrySerializer
+
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+# from rest_framework.exceptions import NotFound
+# from rest_framework.status import HTTP_204_NO_CONTENT
+# from rest_framework.views import APIView
+from .serializers import CategorySerializer
+from rest_framework.viewsets import ModelViewSet
 
 # Create your views here.
 
 
+class CategoryViewSet(ModelViewSet):
+
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+
+""" 2nd deletion
+class Categories(APIView):
+
+    def get(self, request):
+        all_categories = Category.objects.all()
+        serializer = CategrySerializer(
+            all_categories, many=True
+        )  # many 옵션이 없으면 AttributeError at /categories/ 발생
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CategrySerializer(data=request.data)
+        if serializer.is_valid():
+            new_category = serializer.save()  # create method return value save
+            return Response(CategrySerializer(new_category).data)  # re-serialize!
+        else:
+            return Response(serializer.errors)
+"""
+
+""" 1st deletion
 @api_view(["GET", "POST"])
 def categories(request):
     if request.method == "GET":
@@ -24,8 +53,39 @@ def categories(request):
             return Response(CategrySerializer(new_category).data)  # re-serialize!
         else:
             return Response(serializer.errors)
+"""
 
+"""  2nd deletion
+class CategoryDetail(APIView):
 
+    def get_object(self, id):
+        try:
+            return Category.objects.get(id=id)
+        except Category.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, id):
+        serializer = CategrySerializer(self.get_object(id))
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        serializer = CategrySerializer(
+            self.get_object(id),
+            data=request.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            updated_category = serializer.save()
+            return Response(CategrySerializer(updated_category).data)
+        else:
+            return Response(serializer.errors)
+
+    def delete(self, request, id):
+        self.get_object(id).delete()
+        return Response(status=HTTP_204_NO_CONTENT)
+"""
+
+""" 1st deletion
 @api_view(["GET", "PUT", "DELETE"])
 def category(request, id):
     try:
@@ -49,3 +109,4 @@ def category(request, id):
     elif request.method == "DELETE":
         category.delete()
         return Response(status=HTTP_204_NO_CONTENT)
+"""
