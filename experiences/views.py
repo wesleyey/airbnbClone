c60bcbac212d3e2c844f2f92119ff38ed0e9ab42
Wesley Kim <wesleykim@Wesleyui-MacBookAir.local>
2024-05-25 16:22:32 +0900

    amenties, perks urls, serializers, views

--- experiences/views.py
+++ experiences/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from rest_framework.status import HTTP_204_NO_CONTENT
+from .models import Perk
+from .serializers import PerkSerializer
 
 # Create your views here.
+
+
+class Perks(APIView):
+
+    def get(self, request):
+        all_perks = Perk.objects.all()
+        serializer = PerkSerializer(all_perks, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        # 유저로부터 받은 데이터를 시리얼라이저에게 전달
+        serializer = PerkSerializer(data=request.data)
+        if serializer.is_valid():
+            new_perk = serializer.save()
+            return Response(PerkSerializer(new_perk).data)
+        else:
+            return Response(serializer.errors)
+
+
+class PerkDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Perk.objects.get(pk=pk)
+        except Perk.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, pk):
+        perk = self.get_object(pk)
+        serializer = PerkSerializer(perk)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        perk = self.get_object(pk)
+        serializer = PerkSerializer(
+            perk,
+            data=request.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            updated_perk = serializer.save()
+            return Response(
+                PerkSerializer(updated_perk).data,
+            )
+        else:
+            return Response(serializer.errors)
+
+    def delete(self, request, pk):
+        perk = self.get_object(pk)
+        perk.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
